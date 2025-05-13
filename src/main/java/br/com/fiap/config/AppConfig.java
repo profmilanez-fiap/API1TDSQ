75068514198ccd68e02b167d7a7bae05258e0e28
Adriano Milanez <pf1588@fiap.com.br>
2025-05-13 12:31:28 -0300

    Commit CORS

--- src/main/java/br/com/fiap/config/AppConfig.java
+++ src/main/java/br/com/fiap/config/AppConfig.java
@@ -3,11 +3,10 @@ package br.com.fiap.config;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("/") // Define o caminho base da API (ex: /bandas, /discos, etc.)
+@ApplicationPath("/")
 public class AppConfig extends ResourceConfig {
 
     public AppConfig() {
-        // Registra os pacotes que contêm seus recursos e filtros
         packages("br.com.fiap.resource", "br.com.fiap.filter");
     }
 }
--- src/main/java/br/com/fiap/filter/CorsFilter.java
+++ src/main/java/br/com/fiap/filter/CorsFilter.java
@@ -13,19 +13,11 @@ public class CorsFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
 
-        // Origem permitida: seu frontend na Vercel
+        // Use "*" para permitir qualquer origem ou especifique sua URL:
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://front1-tdsq-2xje.vercel.app");
-
-        // Cabeçalhos permitidos
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-
-        // Permitir credenciais (cookies, tokens etc.)
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-
-        // Métodos HTTP permitidos
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-
-        // Tempo que a resposta do CORS pode ser armazenada em cache (em segundos)
         responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
     }
 }

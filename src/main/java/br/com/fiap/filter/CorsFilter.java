54368922570cb837358f38946b77eeddbd5ff9b6
Adriano Milanez <pf1588@fiap.com.br>
2025-05-13 12:12:33 -0300

    Novo CORS

--- src/main/java/br/com/fiap/filter/CorsFilter.java
+++ src/main/java/br/com/fiap/filter/CorsFilter.java
@@ -13,11 +13,19 @@ public class CorsFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
 
+        // Origem permitida: seu frontend na Vercel
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://front1-tdsq-2xje.vercel.app");
+
+        // Cabeçalhos permitidos
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+
+        // Permitir credenciais (cookies, tokens etc.)
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+
+        // Métodos HTTP permitidos
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
+        // Tempo que a resposta do CORS pode ser armazenada em cache (em segundos)
         responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
     }
-    
 }

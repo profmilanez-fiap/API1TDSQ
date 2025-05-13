414d95e1145e0a4393582a73551906314ca4bcf7
Adriano Milanez <pf1588@fiap.com.br>
2025-05-13 12:06:17 -0300

    Atualização CORS VERCEL

--- src/main/java/br/com/fiap/filter/CorsFilter.java
+++ src/main/java/br/com/fiap/filter/CorsFilter.java
@@ -13,7 +13,7 @@ public class CorsFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
 
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://front1-tdsq-2xje.vercel.app");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");

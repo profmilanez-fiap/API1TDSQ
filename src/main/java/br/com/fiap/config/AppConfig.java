502754512ef3f4fc180090dc8bc06f70d041c8ea
Adriano Milanez <pf1588@fiap.com.br>
2025-05-13 14:24:04 -0300

    novo cors

--- src/main/java/br/com/fiap/config/AppConfig.java
+++ src/main/java/br/com/fiap/config/AppConfig.java
@@ -7,9 +7,8 @@ import br.com.fiap.filter.CorsFilter;
 
 @ApplicationPath("/")
 public class AppConfig extends ResourceConfig {
-
     public AppConfig() {
-        packages("br.com.fiap.resource"); // Registra os recursos
-        register(CorsFilter.class);       // Registra explicitamente o filtro
+        packages("br.com.fiap.resource", "br.com.fiap.filter");
     }
 }
+
